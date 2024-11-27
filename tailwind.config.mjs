d098bf6abacecee9749d6126539f371bac4d5ba0
Oliver <oliviertina29@gmail.com>
2024-11-27 15:13:44 +0000

    1st commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],
+      },
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#06b6d4',
+      },
+    },
+  },
+  plugins: [],
+}
